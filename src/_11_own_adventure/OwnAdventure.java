93529ed75cc1d97d4f6ba02349e2d92dbd0d97a6
MichaelYeh507 <54426940+MichaelYeh507@users.noreply.github.com>
2019-11-22 12:22:39 -0800

    ownAdventure

--- src/_11_own_adventure/OwnAdventure.java
+++ src/_11_own_adventure/OwnAdventure.java
@@ -25,8 +25,20 @@ public class OwnAdventure {
 			if (transportation == 0) {
 				//level 3 
 				JOptionPane.showMessageDialog(null, "Congrats you reached another checkpoint!");
+				JOptionPane.showMessageDialog(null, "You are now on the final stage of the woods");
+				JOptionPane.showMessageDialog(null, "You see a nether portal and a door that says exit");
+				int portals = JOptionPane.showOptionDialog(null, "Which way would you go?", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Nether", "exit" }, null);
+				if (portals == 1) {
+					JOptionPane.showMessageDialog(null, "You past the forest! Congrats!");
+				} else {
+					JOptionPane.showMessageDialog(null, "You spawned in lava");
+					JOptionPane.showMessageDialog(null, "You died GG");
+				}
+				
 				
 			} else {
+				// Take D L
 				JOptionPane.showMessageDialog(null, "You took the jetpack and started flying across the river");
 				JOptionPane.showMessageDialog(null, "You did not see that the jetpack was low on fuel");
 				JOptionPane.showMessageDialog(null, "You fall into the river and drowned GG");
